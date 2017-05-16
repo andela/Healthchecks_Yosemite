9aacb134b5a0f72837f4b190f161da8921c1ceef
scotty-b <businge.scott@andela.com>
2017-05-16 13:50:41 +0300

    add test for making sure team acces works for added a check

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -3,7 +3,6 @@ from hc.test import BaseTestCase
 
 
 class AddCheckTestCase(BaseTestCase):
-
     def test_it_works(self):
         url = "/checks/add/"
         self.client.login(username="alice@example.org", password="password")
@@ -11,4 +10,13 @@ class AddCheckTestCase(BaseTestCase):
         self.assertRedirects(r, "/checks/")
         assert Check.objects.count() == 1
 
-    ### Test that team access works
+    # Test that team access works
+    def test_team_access_works(self):
+        url = "/checks/add/"
+        self.client.login(username="bob@example.org", password="password")
+        self.client.post(url)
+
+        check = Check.objects.get()
+        # Confirming that, this check has been created by bob
+        # Since Bob and Alice share channels, then Alice should be able to access any check that is created by bob
+        self.assertEqual(check.user, self.bob)
