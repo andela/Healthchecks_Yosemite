e763698e7623d99b9a2baf1bcd118d4c34ee31f9
jimmykimani <james.kimani@andela.com>
2017-05-16 09:36:51 +0300

    (Feature #145027455) Add assertion in check_model file

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -38,3 +38,8 @@ class CheckModelTestCase(TestCase):
         self.assertFalse(check.in_grace_period())
 
     ### Test that when a new check is created, it is not in the grace period
+     
+    def test_check_created_is_not_in_grace_period(self):
+        
+        check = Check()
+        self.assertFalse(check.in_grace_period())
