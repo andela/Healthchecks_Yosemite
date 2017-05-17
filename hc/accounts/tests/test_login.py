2e1ba073b3521dfdb769bdb9af728f25af3866a9
Daisy Macharia <daisymacharia@mickey.local>
2017-05-17 16:17:54 +0300

    (Feature #145027455) Add asserts to check that new user is created, email body is correct and check is associated with new user

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -19,18 +19,20 @@ class LoginTestCase(TestCase):
         r = self.client.post("/accounts/login/", form)
         assert r.status_code == 302
         # Assert that a user was created
-        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual("alice@example.org", User.objects.get(pk=1).email)
 
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
-        self.assertIn(mail.outbox[0].body, 'To log into healthchecks.io, \
-                       please open the link below: ')
         # Assert contents of the email body
+        self.assertIn('To log into healthchecks.io, ', mail.outbox[0].body)
+
 
         # Assert that check is associated with the new user
         check_again = Check.objects.get(code=session["welcome_code"])
-        assert check_again.user
+        self.assertEqual(session["welcome_code"].replace('-', ''),
+                         Check.objects.filter(user_id=1)[0].code.hex)
+        #assert check_again.user
 
     def test_it_pops_bad_link_from_session(self):
         self.client.session["bad_link"] = True
