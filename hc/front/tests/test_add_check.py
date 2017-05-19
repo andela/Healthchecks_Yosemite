f6ee7b30f0a9ac8885cd0a806407850327bf7c9d
scotty-b <businge.scott@andela.com>
2017-05-19 18:23:29 +0300

    Refactor test add check file

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -13,13 +13,13 @@ class AddCheckTestCase(BaseTestCase):
     # Test that team access works
     def test_team_access_works(self):
         url = "/checks/add/"
-        self.client.login(username="bob@example.org", password="password")
+        self.client.login(username="alice@example.org", password="password")
         self.client.post(url)
 
         check = Check.objects.get()
         # Confirming that, this check has been created by bob
         # Since Bob and Alice share channels, then Alice should be able to access any check that is created by bob
-        self.assertEqual(check.user, self.bob)
+        self.assertEqual(check.user, self.alice)
 
 
 
